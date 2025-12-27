f9aa306227762ce98d4e91678232b90bdb8fa19c
gis-14186840 <zhiwei.wu-3@postgrad.manchester.ac.uk>
2025-12-27 15:26:31 +0000

    update student number&data load

--- assessment2.py
+++ assessment2.py
@@ -1,6 +1,6 @@
 """
 Understanding GIS: Assessment 2
-@author [INSERT STUDENT NUMBER HERE]
+@author 14186840
 """
 from time import perf_counter
 
@@ -9,8 +9,18 @@ start_time = perf_counter()
 
 # --- NO CODE ABOVE HERE ---
 
-
 ''' --- ALL CODE MUST BE INSIDE HERE --- '''
+
+# Import required libraries
+
+
+# 1.Data load
+PARAMS = {
+    'tweet_path': './data/wr/level3-tweets-subset.shp', 
+    'pop_raster_path': './data/wr/100m_pop_2019.tif', 
+    'district_path': './data/wr/gm-districts.shp',}
+
+
 
 
 # --- NO CODE BELOW HERE ---
